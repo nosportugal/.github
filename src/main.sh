23d7172f234502a6095323a788a5408d84754c83
Pedro Reis <pedro.malheiro@parceiros.nos.pt>
2024-04-08 11:36:21 +0100

    ✨ New Logic to use technologies instead of names

---
+++ src/main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get all repos
+repos=$(gh api --paginate orgs/nosportugal/repos | jq -r '.[].name')
+
+# For each repo, get the languages used
+# Use xargs to run multiple processes at the same time
+# -P specifies the maximum number of processes that can be run at the same time
+# -I {} replaces {} with the repo name in the command
+echo $repos | xargs -n 1 -P 10 -I {} bash -c 'gh api repos/nosportugal/{}/languages | jq -r 'keys[]' >> repo_languages.txt'
