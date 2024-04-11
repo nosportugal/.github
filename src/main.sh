275f9af5d7357bce8c4602d2edc92940ad352b16
Pedro Reis <pedro.malheiro@parceiros.nos.pt>
2024-04-11 18:07:20 +0100

    🔧 Converted to GNU xargs

--- src/main.sh
+++ src/main.sh
@@ -7,4 +7,4 @@ repos=$(gh api --paginate orgs/nosportugal/repos | jq -r '.[].name')
 # Use xargs to run multiple processes at the same time
 # -P specifies the maximum number of processes that can be run at the same time
 # -I {} replaces {} with the repo name in the command
-echo $repos | xargs -n 1 -P 10 -I {} bash -c 'gh api repos/nosportugal/{}/languages | jq -r 'keys[]' >> repo_languages.txt'
+echo $repos | xargs -n 1 -P10 bash -c 'gh api repos/nosportugal/$1/languages | jq -r 'keys[]' >> repo_languages.txt'
